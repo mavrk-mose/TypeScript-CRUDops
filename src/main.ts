01f53b74a1962965ef9ee7f24a4c575060ea69a4
mavrk-mose <kippemoses25@gmail.com>
2023-01-06 12:07:40 +0300

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,9 @@ interface Todo {
 
 const list = document.querySelector<HTMLUListElement>('#list');
 const input = document.querySelector<HTMLInputElement>('#new-todo');
-const form = document.querySelector<HTMLFormElement>('todo-form');
+const form = document.querySelector<HTMLFormElement>('#todo-form');
+const todos: Todo[] = loadTodos()
+todos.forEach(addListItem)
 
 //adding an event listener to the form
 form?.addEventListener('submit', (e) => {
@@ -23,7 +25,8 @@ form?.addEventListener('submit', (e) => {
     completed: false,
     createdAt: new Date()
   } 
-   
+  todos.push(newTodo)
+  saveTodos()
   addListItem(newTodo);
 })
 
@@ -31,12 +34,28 @@ function addListItem(todo: Todo) {
     const item = document.createElement('li');
     const label = document.createElement('label');
     const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
+
+    checkbox.addEventListener("change", () => {
+      todo.completed = checkbox.checked
+      saveTodos()
+    })
+    checkbox.type = "checkbox"
+    checkbox.checked = todo.completed;
 
     label.append(checkbox, todo.text);
     item.append(label);
     list?.append(item); 
-
 }
+
+function saveTodos() {
+  localStorage.setItem("TODOs", JSON.stringify(todos))
+}
+
+function loadTodos(): Todo[] {
+  const taskJSON = localStorage.getItem("TODOs")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
+
 
 
